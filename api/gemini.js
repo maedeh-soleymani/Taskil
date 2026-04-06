7fa7a3007b5a1859c12b050191f7f62b11b96bb6
Maedeh Soleymani <soleymani.maedeh93@gmail.com>
2026-04-06 18:24:42 +0300

    Edit AI API

--- api/gemini.js
+++ api/gemini.js
@@ -2,7 +2,7 @@
 export default async function handler(req, res) {
   try {
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent",
       {
         method: "POST",
         headers: {
